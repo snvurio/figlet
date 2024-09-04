f8a48e1a62a942f651cdfd7c56673023a30daf3f
상혁 <pp790516@naver.com>
2024-09-04 17:16:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet"); //figlet불러오기
 
-figlet("guehyun real ugly", function (err, data) {
+figlet("Hello World!!", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
